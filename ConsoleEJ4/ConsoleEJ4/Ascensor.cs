357ef51d600ac52b4623b263d26bcbe7274f018e
DAR-LAB-MA\UCSE <a>
2022-05-11 19:54:33 -0300

    Commit EJ4

---
+++ ConsoleEJ4/ConsoleEJ4/Ascensor.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleEJ4
+{
+    //Cree una clase Ascensor que tenga los métodos.
+    //DefinirPiso(int pisoNuevo) - público
+    //subirPiso() - privado
+    //bajarPiso() - privado
+    //LlamarAscensor(int pisoActual)- público
+    public class PisoArgs : EventArgs
+    {
+        public int Piso { get; set; }
+        public string Mensaje { get; set; }
+    }
+    public class Ascensor
+    {
+        private delegate bool DelegLlamarAscensor();
+        private delegate bool DelegDefinirPiso();
+
+        public EventHandler<PisoArgs> PisoAhora;
+        public int PisoActual { get; set; }
+        //public int CantidadDePisos { get; set;}
+        public Ascensor(int pisoActual)//, int cantidadDePisos)
+        {
+            PisoActual = pisoActual;
+           //CantidadDePisos = cantidadDePisos;
+        }
+        private void SubirPiso()
+        {
+            PisoActual++;
+            this.PisoAhora(this, new PisoArgs
+            {
+                Piso = PisoActual
+            });
+            //if (PisoActual>CantidadDePisos)
+            //    return false;
+            //return true;
+        }        
+        private void BajarPiso()
+        {
+            PisoActual--;
+            this.PisoAhora(this, new PisoArgs
+            {
+                Piso = PisoActual
+            });
+            //if (PisoActual<0)
+            //    return false;
+            //return true;
+        }
+        public void LlamarAscensor(int piso)
+        {
+            while (PisoActual-1!=piso)
+            {
+                if (piso > PisoActual)
+                    SubirPiso();
+                else
+                    BajarPiso();
+            }
+            this.PisoAhora(this, new PisoArgs
+            {
+                Mensaje = "El ascensor llegó a su piso"
+            });
+        }
+        public void DefinirPiso(int pisoDeseado)
+        {
+            int diferencia;
+            if (pisoDeseado>PisoActual)
+            {
+                diferencia = pisoDeseado - PisoActual;
+                for (int i = 1; i < diferencia; i++)
+                {
+                    SubirPiso();
+                }
+            }
+            else if(pisoDeseado<PisoActual)
+            {
+                diferencia = PisoActual - pisoDeseado;
+                for (int i = 1; i < diferencia; i++)
+                {
+                    BajarPiso();
+                }
+            }
+            else
+            {
+                this.PisoAhora(this, new PisoArgs
+                {
+                    Mensaje = "Llego a su piso destino"
+                });
+            }
+        }
+    }
+}
---
+++ ConsoleEJ4/ConsoleEJ4/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace ConsoleEJ4
+{
+    internal class Program
+    {
+        //Cree una clase Ascensor que tenga los métodos.
+        //DefinirPiso(int pisoNuevo) - público
+        //subirPiso() - privado
+        //bajarPiso() - privado
+        //LlamarAscensor(int pisoActual)- público
+
+        //Internamente los métodos públicos deben usar únicamente los métodos privados.
+        //Los métodos privados únicamente pueden subir o bajar 1 piso a la vez.
+
+        //Dispare un evento cada vez que cambie el piso en el que está el ascensor.
+        //Investigue y utilice Thread.Sleep(1000).
+
+        //En el manejador del evento imprima el piso actual, con el objetivo de que su visualización sea idéntica al display del ascensor en la realidad.
+        //Console.Clear() - Borra la consola.
+        //Console.Foreground y similares.
+
+        static void Main(string[] args)
+        {
+            Ascensor ascensor = new Ascensor(0);
+            ascensor.PisoAhora += PisoHandler;
+            bool loop = true;
+            while (loop)
+            {
+                Console.Write("Comandos del auto:\n1 | Llamar Ascensor\n2 | Definir Piso\n9 | Salir\nEleccion: ");
+                int nro = int.Parse(Console.ReadLine());
+                switch (nro)
+                {
+                    case 1:
+                        Console.WriteLine("Ingrese su piso: ");
+                        ascensor.LlamarAscensor(int.Parse(Console.ReadLine()));
+                        Console.Clear();
+                        break;
+                    case 2:
+                        Console.WriteLine("Ingrese su piso destino: ");
+                        ascensor.DefinirPiso(int.Parse(Console.ReadLine()));
+                        Console.Clear();
+                        break;
+                    case 9:
+                        loop = false;
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
+        public static void PisoHandler(object sender, PisoArgs pisoArgs)
+        {
+            Console.WriteLine($"\nPiso Actual: {pisoArgs.Piso} {pisoArgs.Mensaje}");
+            Thread.Sleep(2000);
+            Console.Clear();
+        }
+    }
+}
