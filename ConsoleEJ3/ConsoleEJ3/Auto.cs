781e6628064eac1d9f45a8df01a9a41c80d0f4a2
DESKTOP-BF35AJB\Usuario <stefmar23ivan@gmail.com>
2022-05-11 17:32:06 -0300

    CommitEj3

---
+++ ConsoleEJ3/ConsoleEJ3/Auto.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleEJ3
+{
+    public class VelocidadArgs : EventArgs
+    {
+        public int limiteVelocidad { get; set; }
+    }
+    class Auto
+    {
+        private delegate bool EncenderDelegado();
+        private delegate bool VelocidadDelegado(int velocidad);
+
+        public EventHandler<VelocidadArgs> VelocidadMaximaSuperada;
+
+        public bool Encendido { get; set; }
+        public int VelActual { get; set; }
+        public int VelMaxima { get; set; }
+        public Auto(bool encendido, int velActual, int velMaxima)
+        {
+            Encendido = encendido;
+            VelActual = velActual;
+            VelMaxima = velMaxima;
+        }
+        public Auto(int velMaxima)
+        {
+            Encendido = false;
+            VelActual = 0;
+            VelMaxima = velMaxima;
+        }
+        private bool Encender()
+        {
+            Encendido = true;
+            return Encendido;
+        }
+        private bool Apagar()
+        {
+            Encendido = false;
+            return true;
+        }
+        private bool SubirVelocidad(int velocidad)
+        {
+            VelActual = velocidad;
+            if (velocidad > VelMaxima)
+            {
+                this.VelocidadMaximaSuperada(this, new VelocidadArgs
+                {
+                    limiteVelocidad = VelMaxima
+                });
+            }
+            return true;
+        }
+        private bool BajarVelocidad(int velocidad)
+        {
+            if (velocidad <= 0)
+                Apagar();
+            else
+                VelActual = velocidad;
+            return true;
+        }
+        public bool Arrancar()
+        {
+            EncenderDelegado encenderDelegado;
+            encenderDelegado = !this.Encendido ? new EncenderDelegado(Encender) : null;
+            return encenderDelegado != null ? encenderDelegado() : false;
+        }
+        public bool ModificarVelocidad(int velocidad)
+        {
+            VelocidadDelegado velocidadDelegado = velocidad >= VelActual ? new VelocidadDelegado(SubirVelocidad) : new VelocidadDelegado(BajarVelocidad);
+            velocidadDelegado(velocidad);
+            return true;
+        }
+    }
+}
---
+++ ConsoleEJ3/ConsoleEJ3/ConsoleMain.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleEJ3
+{
+    class ConsoleMain
+    {
+        static void Main(string[] args)
+        {
+            Auto auto = new Auto(150);
+            auto.VelocidadMaximaSuperada += VelocidadMaximaHandler;
+            bool loop = true;
+            while (loop)
+            {
+                Console.Write("Comandos del auto:\n1 | Arrancar\n2 | Modificar Velocidad\n9 | Salir\nEleccion: ");
+                int nro = int.Parse(Console.ReadLine());
+                switch (nro)
+                {
+                    case 1:
+                        auto.Arrancar();
+                        break;
+                    case 2:
+                        Console.WriteLine("Ingrese velocidad: ");
+                        auto.ModificarVelocidad(int.Parse(Console.ReadLine()));
+                        break;
+                    case 9:
+                        loop = false;
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
+        public static void VelocidadMaximaHandler(object sender, VelocidadArgs velocidadArgs)
+        {
+            Console.WriteLine($"\nLa velocidad máxima ({velocidadArgs.limiteVelocidad}km/h) ha sido sobrepasada. ");
+        }
+    }
+}
